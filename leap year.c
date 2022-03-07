82d486ebc654e5933c5a522dd3cde6a1b88d4cea
YUTIANLE <100507052+YUTIANLEXX@users.noreply.github.com>
2022-03-07 21:23:08 +0800

    Create leap year.c

---
+++ leap year.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main()
+{
+	int count = 0;
+	int year = 0;
+	for (year = 1000; year <= 2000; year++)
+	{
+		if (year % 4 == 0 && year % 100 != 0)
+		{
+			printf("%d ", year);
+			count++;
+		}
+		else if (year % 400 == 0)
+		{
+			printf("%d ", year);
+			count++;
+		}
+	}
+	printf("count=%d\n", count);
+	return 0;
+}
