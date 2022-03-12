601a81e4f2f880fa9df371cd502c340d0b47611d
YUTIANLE <100507052+YUTIANLEXX@users.noreply.github.com>
2022-03-12 21:23:03 +0800

    Update leap year.c

--- leap year.c
+++ leap year.c
@@ -19,3 +19,24 @@ int main()
 	printf("count=%d\n", count);
 	return 0;
 }
+//Another way to do it
+//#include <stdio.h>
+//int is_leap(int y)
+//{
+//	if ((y % 4 == 0 && y % 100 != 0) || (y % 400 == 0))
+//	{
+//		return 1;
+//	}
+//	else
+//		return 0;
+//}
+//int main()
+//{
+//	int year = 0;
+//	for (year = 1000; year <= 2000; year++)
+//	{
+//		if (1 == is_leap(year))
+//			printf("%d ", year);
+//	}
+//	return 0;
+//}
