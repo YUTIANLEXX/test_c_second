4ae4f9508bd44b4e827f21d7e2eb30568b3c8344
YUTIANLE <100507052+YUTIANLEXX@users.noreply.github.com>
2022-03-12 21:23:05 +0800

    Update prime number two.c

--- prime number two.c
+++ prime number two.c
@@ -22,3 +22,25 @@ int main()
 	printf("\ncount=%d\n", count);
 	return 0;
 }
+//Another way to do it
+//#include <stdio.h>
+//int is_prime(int n)
+//{
+//	int j = 0;
+//	for (j = 2; j < n; j++)
+//	{
+//		if (n%j == 0)
+//			return 0;
+//	}
+//	return 1;
+//}
+//int main()
+//{
+//	int i = 0;
+//	for (i = 100; i <= 200; i++)
+//	{
+//		if (1 == is_prime(i))
+//			printf("%d ", i);
+//	}
+//	return 0;
+//}
