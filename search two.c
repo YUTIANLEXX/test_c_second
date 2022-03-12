080a735678889f80ff347087c0ae60262f79f656
YUTIANLE <100507052+YUTIANLEXX@users.noreply.github.com>
2022-03-12 21:23:07 +0800

    Update search two.c

--- search two.c
+++ search two.c
@@ -29,3 +29,43 @@ int main()
 	}
 	return 0;
 }
+//Another way to do it
+//#include <stdio.h>
+//int binary_search(int arr[], int k, int sz)
+//{
+//	int left = 0;
+//	int right = sz - 1;
+//	int mid = (left + right) / 2;
+//	while (left <= right)
+//	{
+//		if (arr[mid] < k)
+//		{
+//			left = mid + 1;
+//		}
+//		else if (arr[mid] > k)
+//		{
+//			right = mid - 1;
+//		}
+//		else
+//		{
+//			return mid;
+//		}
+//	}
+//	return -1;
+//}
+//int main()
+//{
+//	int arr[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+//	int k = 7;
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	int ret = binary_search(arr, k, sz);
+//	if (ret == -1)
+//	{
+//		printf("找不到\n");
+//	}
+//	else
+//	{
+//		printf("找到了，下标是：%d\n", ret);
+//	}
+//	return 0;
+//}
