bff5c50a73bc5ab40979ba012735afb2b53ec38e
YUTIANLE <100507052+YUTIANLEXX@users.noreply.github.com>
2022-03-16 20:58:18 +0800

    Update factorial n!.c

--- factorial n!.c
+++ factorial n!.c
@@ -12,3 +12,51 @@ int main()
 	printf("ret=%d\n", ret);
 	return 0;
 }
+
+//Another way to do it
+//#include <stdio.h>
+//int Fac(int n)
+//{
+//	int i = 0;
+//	int ret = 1;
+//	for (i = 1; i <= n; i++)
+//	{
+//		ret *= i;
+//	}
+//	return ret;
+//}
+//int main()
+//{
+//	int n = 0;
+//	int ret = 0;
+//	printf("请输入要求的阶乘的整数：");
+//	scanf("%d", &n);
+//	ret = Fac(n);
+//	printf("%d\n", ret);
+//	return 0;
+//}
+
+//Another way to do it
+//#include <stdio.h>
+//int Fac(int n)
+//{
+//	if (n <= 1)
+//	{
+//		return 1;
+//	}
+//	else
+//	{
+//		return n*Fac(n - 1);
+//	}
+//
+//}
+//int main()
+//{
+//	int n = 0;
+//	int ret = 0;
+//	printf("请输入要求的阶乘的整数：");
+//	scanf("%d", &n);
+//	ret = Fac(n);
+//	printf("%d\n", ret);
+//	return 0;
+//}
